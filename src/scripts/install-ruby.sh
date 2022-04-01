5ab72570f87403bb4f68c50e4f5f6da57e15c9ca
Eric Ribeiro <ericribeiro@outlook.com.br>
2022-04-01 12:14:05 -0700

    fix: changed syntax to comply with shellcheck

--- src/scripts/install-ruby.sh
+++ src/scripts/install-ruby.sh
@@ -2,4 +2,4 @@
 
 rvm install "$PARAM_VERSION"
 rvm use "$PARAM_VERSION"
-echo . "$(rvm $PARAM_VERSION; do rvm env --path)" >> $BASH_ENV
+echo . $(rvm "$PARAM_VERSION" 1> /dev/null 2> /dev/null && rvm env --path) >> $BASH_ENV
