385c209762766d35c96dbd4d035fdba0d9bf7fe7
Eric Ribeiro <ericribeiro@outlook.com.br>
2022-04-01 12:16:52 -0700

    fix: split bash env sourcing to make it more readable

--- src/scripts/install-ruby.sh
+++ src/scripts/install-ruby.sh
@@ -2,4 +2,6 @@
 
 rvm install "$PARAM_VERSION"
 rvm use "$PARAM_VERSION"
-echo . $(rvm "$PARAM_VERSION" 1> /dev/null 2> /dev/null && rvm env --path) >> $BASH_ENV
+
+readonly ruby_path="$(rvm $PARAM_VERSION 1> /dev/null 2> /dev/null && rvm env --path)"
+printf '%s\n' ". $ruby_path" >> $BASH_ENV
