56bd5d62e453e14fc26768684c39514778a9a98c
Eric Ribeiro <ericribeiro@outlook.com.br>
2022-04-01 16:34:11 -0700

    add x flag

--- src/scripts/rspec-test.sh
+++ src/scripts/rspec-test.sh
@@ -13,5 +13,7 @@ IFS=","
 read -ra globs <<< "$PARAM_INCLUDE"
 IFS="$old_ifs"
 
+set -x
+
 readonly TESTFILES=$(circleci tests glob "${globs[@]}" | circleci tests split --split-by=timings)
 bundle exec rspec "$TESTFILES" --profile 10 --format RspecJunitFormatter --out "$PARAM_OUT_PATH"/results.xml --format progress
