7b3d59d1bcf2b5aa3c4489a6526f3c9c3736dccc
Eric Ribeiro <ericribeiro@outlook.com.br>
2022-04-01 12:25:00 -0700

    refactor: change to more readable syntax

--- src/scripts/install-ruby.sh
+++ src/scripts/install-ruby.sh
@@ -4,4 +4,4 @@ rvm install "$PARAM_VERSION"
 rvm use "$PARAM_VERSION"
 
 readonly ruby_path="$(rvm $PARAM_VERSION 1> /dev/null 2> /dev/null && rvm env --path)"
-printf '%s\n' ". $ruby_path" >> $BASH_ENV
+printf '%s\n' "source $ruby_path" >> $BASH_ENV
