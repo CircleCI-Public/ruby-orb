CircleCI-Public/ruby-orb

commit aca9e3a996d7f39e906a3cf50e09867d070437e4

Make sure we quote the glob to properly expand it (#93) [semver:patch]

src/commands/rspec-test.yml
@@ -17,7 +17,7 @@ steps:
         name: <<parameters.label>>
         command: |
             mkdir -p <<parameters.out-path>>
-            TESTFILES=$(circleci tests glob <<parameters.include>> | circleci tests split --split-by=timings)
+            TESTFILES=$(circleci tests glob "<<parameters.include>>" | circleci tests split --split-by=timings)
             bundle exec rspec $TESTFILES --profile 10 --format RspecJunitFormatter --out <<parameters.out-path>>/results.xml --format progress
     - store_test_results:
         path: <<parameters.out-path>>

commit f9e13b9852e8103d87fd192e30a3c2d4ab79de5a

fix: add the ability to pass multiple globs as parameter
This change will fix the bug introduced in #93
by introducing support of a comma separated
list of globs.

src/commands/rspec-test.yml
@@ -2,7 +2,9 @@ description: "Test with RSpec. You have to add `gem 'rspec_junit_formatter'` to 
 parameters:
     include:
         default: "spec/**/*_spec.rb"
-        description: Glob to define where your test files are kept within your repository.
+        description: >
+            Glob to define where your test files are kept within your repository.
+            Should multiple globs be required, they must be passed in a comma separated string (e.g.: "spec/**/*_spec.rb,spec2/**/*_spec.rb"). 
         type: string
     label:
         default: "RSpec Tests"
@@ -20,10 +22,10 @@ parameters:
 steps:
     - run:
         name: <<parameters.label>>
-        command: |
-            mkdir -p <<parameters.out-path>>
-            TESTFILES=$(circleci tests glob "<<parameters.include>>" | circleci tests split --split-by=timings)
-            bundle exec rspec $TESTFILES --profile 10 --format RspecJunitFormatter --out <<parameters.out-path>>/results.xml --format progress
+        environment:
+            PARAM_OUT_PATH: <<parameters.out-path>>
+            PARAM_INCLUDE: <<parameters.include>>
+        command: <<include(scripts/rspec-test.sh)>>
         working_directory: <<parameters.app-dir>>
     - store_test_results:
         path: <<parameters.out-path>>

src/scripts/rspec-test.sh
@@ -1,0 +1,30 @@
+#!/usr/bin/env bash
+
+quoted_globs=""
+
+function quote_globs() {
+  local IFS=","
+
+  read -ra params <<< "$PARAM_INCLUDE"
+
+  for param in "${params[@]}"; do
+    if [ -z "$quoted_globs" ]; then
+      quoted_globs="\"$param\""
+    else
+      quoted_globs="$quoted_globs \"$param\""
+    fi
+  done
+}
+
+if ! quote_globs; then
+  printf '%s\n' "Failed to quote globs: \"$PARAM_INCLUDE\""
+  exit 1
+fi
+
+if ! mkdir -p "$PARAM_OUT_PATH"; then
+  printf '%s\n' "Failed to create output directory: \"$PARAM_OUT_PATH\""
+  exit 1
+fi
+
+readonly TESTFILES=$(circleci tests glob "$quoted_globs" | circleci tests split --split-by=timings)
+bundle exec rspec "$TESTFILES" --profile 10 --format RspecJunitFormatter --out "$PARAM_OUT_PATH"/results.xml --format progress
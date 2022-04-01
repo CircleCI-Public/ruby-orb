f9e13b9852e8103d87fd192e30a3c2d4ab79de5a
Eric Ribeiro <ericribeiro@outlook.com.br>
2022-04-01 10:29:33 -0700

    fix: add the ability to pass multiple globs as parameter
    This change will fix the bug introduced in #93
    by introducing support of a comma separated
    list of globs.

---
+++ src/scripts/rspec-test.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+quoted_globs=""
+
+function quote_globs() {
+  local IFS=","
+
+  read -ra params <<< "$PARAM_INCLUDE"
+
+  for param in "${params[@]}"; do
+    if [ -z "$quoted_globs" ]; then
+      quoted_globs="\"$param\""
+    else
+      quoted_globs="$quoted_globs \"$param\""
+    fi
+  done
+}
+
+if ! quote_globs; then
+  printf '%s\n' "Failed to quote globs: \"$PARAM_INCLUDE\""
+  exit 1
+fi
+
+if ! mkdir -p "$PARAM_OUT_PATH"; then
+  printf '%s\n' "Failed to create output directory: \"$PARAM_OUT_PATH\""
+  exit 1
+fi
+
+readonly TESTFILES=$(circleci tests glob "$quoted_globs" | circleci tests split --split-by=timings)
+bundle exec rspec "$TESTFILES" --profile 10 --format RspecJunitFormatter --out "$PARAM_OUT_PATH"/results.xml --format progress
