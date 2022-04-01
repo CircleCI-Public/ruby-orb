be901ea84b32682932eae08d8bd7abda89dd927b
Eric Ribeiro <ericribeiro@outlook.com.br>
2022-04-01 16:40:10 -0700

    add echo

--- src/scripts/rspec-test.sh
+++ src/scripts/rspec-test.sh
@@ -14,6 +14,11 @@ read -ra globs <<< "$PARAM_INCLUDE"
 IFS="$old_ifs"
 
 set -x
-circleci tests glob "${globs[@]}"
+
+readonly test_files=$(circleci tests glob "${globs[@]}")
+
+echo "!!!!"
+echo "$test_files"
+
 readonly TESTFILES=$(circleci tests glob "${globs[@]}" | circleci tests split --split-by=timings)
-bundle exec rspec "$TESTFILES" --profile 10 --format RspecJunitFormatter --out "$PARAM_OUT_PATH"/results.xml --format progress
+# bundle exec rspec "$TESTFILES" --profile 10 --format RspecJunitFormatter --out "$PARAM_OUT_PATH"/results.xml --format progress
