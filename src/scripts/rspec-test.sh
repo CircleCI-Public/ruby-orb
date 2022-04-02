dfdc528fcf0c25cb591b82981a441ee95bd68828
Eric Ribeiro <ericribeiro@outlook.com.br>
2022-04-01 17:06:00 -0700

    rollback changes

--- src/scripts/rspec-test.sh
+++ src/scripts/rspec-test.sh
@@ -1,33 +1,22 @@
 #!/usr/bin/env bash
 
+set -e
+
 if ! mkdir -p "$PARAM_OUT_PATH"; then
   printf '%s\n' "Failed to create output directory: \"$PARAM_OUT_PATH\""
   exit 1
 fi
 
 # Backup IFS and set it to comma
-# readonly old_ifs="$IFS"
-# IFS=","
+readonly old_ifs="$IFS"
+IFS=","
 
-# Split globs per comma and rollback IFS
-# read -ra globs <<< "$PARAM_INCLUDE"
-# IFS="$old_ifs"
+# Split globs per comma and change IFS to space
+read -ra globs <<< "$PARAM_INCLUDE"
+IFS=" "
 
-# IFS=" "
-# my_globs="${globs[*]}"
+# Run CLI command with glob files separated by space and rollback IFS
+test_files=$(circleci tests glob "${globs[*]}" | circleci tests split --split-by=timings)
+IFS="$old_ifs"
 
-files="$(circleci tests glob "$PARAM_INCLUDE")"
-
-echo "!!!!!!"
-echo $files
-
-split="$(echo $files | circleci tests split --split-by=timings)"
-
-echo "######"
-echo "$"
-
-readonly TESTFILES=$(circleci tests glob "$split" | circleci tests split --split-by=timings)
-
-echo "$TESTFILES"
-
-bundle exec rspec "$TESTFILES" --profile 10 --format RspecJunitFormatter --out "$PARAM_OUT_PATH"/results.xml --format progress
+bundle exec rspec "$test_files" --profile 10 --format RspecJunitFormatter --out "$PARAM_OUT_PATH"/results.xml --format progress
