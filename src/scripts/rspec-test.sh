cf45fcb0228cf981a7db48669c617af506b45b0e
Eric Ribeiro <ericribeiro@outlook.com.br>
2022-04-04 12:34:57 -0700

    trying something

--- src/scripts/rspec-test.sh
+++ src/scripts/rspec-test.sh
@@ -7,18 +7,21 @@ if ! mkdir -p "$PARAM_OUT_PATH"; then
   exit 1
 fi
 
-# Backup IFS and set it to comma
+# Backup IFS
 readonly old_ifs="$IFS"
-IFS=","
 
-# Split globs per comma and rollback IFS
-read -ra globs <<< "$PARAM_INCLUDE"
-IFS="$old_ifs"
+# Split globs per comma
+IFS="," read -ra globs <<< "$PARAM_INCLUDE"
 
 split_files=$(circleci tests glob "${globs[@]}" | circleci tests split)
 
-echo "!!!!!"
-# shellcheck disable=SC2116
-TEMP=$(echo "$split_files")
+# Convert list of test files to array
+IFS=$'\n' read -rd '' -a test_files <<< "$split_files"
 
-bundle exec rspec "$TEMP" --profile 10 --format RspecJunitFormatter --out "$PARAM_OUT_PATH"/results.xml --format progress
+# Parse array of test files to string separated by single space and run tests
+IFS=" "
+test_files_param="${test_files[*]}" 
+bundle exec rspec "$test_files_param" --profile 10 --format RspecJunitFormatter --out "$PARAM_OUT_PATH"/results.xml --format progress
+
+# Rollback IFS
+IFS="$old_ifs"
