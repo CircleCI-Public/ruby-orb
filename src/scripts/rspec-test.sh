ee11c434b2570cb7315541483a011d9840fac87a
Eric Ribeiro <ericribeiro@outlook.com.br>
2022-04-04 10:01:43 -0700

    testing split by name

--- src/scripts/rspec-test.sh
+++ src/scripts/rspec-test.sh
@@ -8,18 +8,13 @@ if ! mkdir -p "$PARAM_OUT_PATH"; then
 fi
 
 # Backup IFS and set it to comma
-readonly old_ifs="$IFS"
+# readonly old_ifs="$IFS"
 IFS=","
 
 # Split globs per comma and rollback IFS
 read -ra globs <<< "$PARAM_INCLUDE"
-
-split_files=$(circleci tests glob "${globs[@]}" | circleci tests split --split-by=timings)
-
-IFS=$'\n'
-read -ra test_files <<< "$split_files"
 IFS="$old_ifs"
 
-echo "${test_files[@]}"
+split_files=$(circleci tests glob "${globs[@]}" | circleci tests split)
 
-bundle exec rspec "${test_files[@]}" --profile 10 --format RspecJunitFormatter --out "$PARAM_OUT_PATH"/results.xml --format progress
+bundle exec rspec "$split_files" --profile 10 --format RspecJunitFormatter --out "$PARAM_OUT_PATH"/results.xml --format progress
