6f8940fb68811e904b3697f66a1be98ea86815bb
Eric Ribeiro <ericribeiro@outlook.com.br>
2022-04-04 19:16:28 -0700

    add split by timings

--- src/scripts/rspec-test.sh
+++ src/scripts/rspec-test.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-set -fx
+# Disable bash glob expansion
+# Without this, the glob parameter will be expanded before the split command is run
+set -o noglob
 
 if ! mkdir -p "$PARAM_OUT_PATH"; then
   printf '%s\n' "Failed to create output directory: \"$PARAM_OUT_PATH\""
@@ -10,21 +12,20 @@ fi
 # Backup IFS
 readonly old_ifs="$IFS"
 
-# Split globs per comma
+# Split globs per comma and run the CLI split command
 IFS="," 
 read -ra globs <<< "$PARAM_INCLUDE"
-
-split_files=$(circleci tests glob "${globs[@]}" | circleci tests split)
-
-printf '%s\n' "$split_files"
+split_files=$(circleci tests glob "${globs[@]}" | circleci tests split --split-by=timings)
 
 # Convert list of test files to array
+# This is necessary because the split command returns a list of files separated by newline
 while IFS= read -r line; do test_files+=("$line"); done <<< "$split_files"
 
+# Rollback IFS
+IFS="$old_ifs"
+
+printf '%s\n' "Running \"bundle exec rspec\" with the follwing files:"
 printf '%s\n' "${test_files[@]}"
 
 # Parse array of test files to string separated by single space and run tests
 bundle exec rspec "${test_files[@]}" --profile 10 --format RspecJunitFormatter --out "$PARAM_OUT_PATH"/results.xml --format progress
-
-# Rollback IFS
-IFS="$old_ifs"
