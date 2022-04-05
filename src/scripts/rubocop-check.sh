3fc0d5d1acf8bd093621989bfc931a8d2a60b6dc
Eric Ribeiro <ericribeiro@outlook.com.br>
2022-04-04 21:15:42 -0700

    replace extra args param with parallel

--- src/scripts/rubocop-check.sh
+++ src/scripts/rubocop-check.sh
@@ -2,8 +2,13 @@
 
 mkdir -p "$PARAM_OUT_PATH"
 
-set -x
-bundle exec rubocop "$PARAM_CHECK_PATH" \
+if [ "$PARAM_PARALLEL" -eq 0]; then
+  bundle exec rubocop "$PARAM_CHECK_PATH" \
   --out $"$PARAM_OUT_PATH"/check-results.xml \
-  --format "$PARAM_FORMAT" \
-  ${PARAM_EXTRA_ARGS:+"$PARAM_EXTRA_ARGS"}
+  --format "$PARAM_FORMAT"
+else
+  bundle exec rubocop "$PARAM_CHECK_PATH" \
+  --out $"$PARAM_OUT_PATH"/check-results.xml \
+  --format "$PARAM_FORMAT"
+  --parallel
+fi
