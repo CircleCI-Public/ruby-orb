2f5641af492e667eff8cb18a8b490e4ae6265229
Eric Ribeiro <ericribeiro@outlook.com.br>
2022-04-04 18:56:11 -0700

    trying other approach

--- src/scripts/rspec-test.sh
+++ src/scripts/rspec-test.sh
@@ -19,14 +19,13 @@ split_files=$(circleci tests glob "${globs[@]}" | circleci tests split)
 printf '%s\n' "$split_files"
 
 # Convert list of test files to array
-IFS=$'\n'
-read -r -a files <<< "$split_files"
+while IFS= read -r line; do test_files+=("$line"); done <<< "$split_files"
 
-printf '%s\n' "${files[@]}"
+printf '%s\n' "${test_files[@]}"
 
 # Parse array of test files to string separated by single space and run tests
 IFS=" "
-test_files_param="${files[*]}" 
+test_files_param="${test_files[*]}" 
 bundle exec rspec "$test_files_param" --profile 10 --format RspecJunitFormatter --out "$PARAM_OUT_PATH"/results.xml --format progress
 
 # Rollback IFS
