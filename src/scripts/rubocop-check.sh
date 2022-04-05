fad91ed9ee27a19401d883c2884807b9729e1c27
Eric Ribeiro <ericribeiro@outlook.com.br>
2022-04-04 21:17:01 -0700

    fix shellcheck

--- src/scripts/rubocop-check.sh
+++ src/scripts/rubocop-check.sh
@@ -2,7 +2,7 @@
 
 mkdir -p "$PARAM_OUT_PATH"
 
-if [ "$PARAM_PARALLEL" -eq 0]; then
+if [ "$PARAM_PARALLEL" -eq 0 ]; then
   bundle exec rubocop "$PARAM_CHECK_PATH" \
   --out $"$PARAM_OUT_PATH"/check-results.xml \
   --format "$PARAM_FORMAT"
