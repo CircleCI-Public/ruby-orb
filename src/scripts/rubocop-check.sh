a4ae273641cdb2cccad3306b4ec028f5ca8ad399
Eric Ribeiro <ericribeiro@outlook.com.br>
2022-04-04 20:25:11 -0700

    feat: add order param

--- src/scripts/rubocop-check.sh
+++ src/scripts/rubocop-check.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 mkdir -p "$PARAM_OUT_PATH"
-bundle exec rubocop "$PARAM_CHECK_PATH" --out $"$PARAM_OUT_PATH"/check-results.xml --format "$PARAM_FORMAT"
+bundle exec rubocop "$PARAM_CHECK_PATH" --out $"$PARAM_OUT_PATH"/check-results.xml --format "$PARAM_FORMAT" --order "$PARAM_ORDER"
