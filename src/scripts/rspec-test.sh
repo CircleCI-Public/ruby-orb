CircleCI-Public/ruby-orb

commit aca9e3a996d7f39e906a3cf50e09867d070437e4

Make sure we quote the glob to properly expand it (#93) [semver:patch]

src/commands/rspec-test.yml
@@ -17,7 +17,7 @@ steps:
         name: <<parameters.label>>
         command: |
             mkdir -p <<parameters.out-path>>
-            TESTFILES=$(circleci tests glob <<parameters.include>> | circleci tests split --split-by=timings)
+            TESTFILES=$(circleci tests glob "<<parameters.include>>" | circleci tests split --split-by=timings)
             bundle exec rspec $TESTFILES --profile 10 --format RspecJunitFormatter --out <<parameters.out-path>>/results.xml --format progress
     - store_test_results:
         path: <<parameters.out-path>>

commit 58b4ffb65bed7d0521f6526e8ab10be775487c4a

feat: Migrate to Orb Tools Orb v11 and Close Multiple Issues (#96)
* fix: add the ability to pass multiple globs as parameter

This change will fix the bug introduced in #93
by introducing a comma-separated list of globs for testing

.circleci/config.yml
@@ -1,140 +1,36 @@
 version: 2.1
-
-references:
-  postgres: &postgres
-    image: circleci/postgres:9.5-alpine
-    environment:
-      POSTGRES_USER: circleci-demo-ruby
-      POSTGRES_DB: rails_blog_test
-      POSTGRES_PASSWORD: ""
-
+setup: true
 orbs:
-  ruby: circleci/ruby@<<pipeline.parameters.dev-orb-version>>
-  orb-tools: circleci/orb-tools@9.1
+  orb-tools: circleci/orb-tools@11.1
+  shellcheck: circleci/shellcheck@3.1
 
-# Pipeline parameters
-parameters:
-  # These pipeline parameters are required by the "trigger-integration-tests-workflow"
-  # job, by default.
-  run-integration-tests:
-    type: boolean
-    default: false
-  dev-orb-version:
-    type: string
-    default: "dev:alpha"
+filters: &filters
+  tags:
+    only: /.*/
 
-jobs:
-  integration-tests:
-    docker:
-      - image: cimg/ruby:2.7-node
-      - *postgres
-    parallelism: 2
-    environment:
-      BUNDLE_JOBS: "3"
-      BUNDLE_RETRY: "3"
-      PGHOST: 127.0.0.1
-      PGUSER: circleci-demo-ruby
-      PGPASSWORD: ""
-      RAILS_ENV: test
-    steps:
-      - run: git clone https://github.com/CircleCI-Public/circleci-demo-ruby-rails.git .
-      - ruby/install-deps:
-          key: "gems-v4"
-      - restore_cache:
-          keys:
-            - rails-demo-yarn-v3-{{ checksum "yarn.lock" }}
-      - run:
-          name: Yarn Install
-          command: yarn install --cache-folder ~/.cache/yarn
-      # Store yarn / webpacker cache
-      - save_cache:
-          key: rails-demo-yarn-v3-{{ checksum "yarn.lock" }}
-          paths:
-            - ~/.cache/yarn
-      - run:
-          name: Wait for DB
-          command: dockerize -wait tcp://localhost:5432 -timeout 1m
-      - run:
-          name: Database setup
-          command: bundle exec rails db:schema:load --trace
-      - ruby/rspec-test
-      - ruby/rubocop-check
-
-  install-on-machine:
-    machine:
-      image: ubuntu-2004:202111-02
-    steps:
-      - ruby/install:
-          version: "2.7.0"
-      - run:
-          name: "Test Install"
-          command: ruby --version | grep "2.7.0"
-  install-on-node:
-      docker:
-        - image: cimg/node:current
-      steps:
-        - ruby/install:
-            version: "2.6"
-        - run:
-            name: "Test Install"
-            command: ruby --version | grep "2.6"
 workflows:
-  # This `lint-pack_validate_publish-dev` workflow will run on any commit.
-  lint_pack-validate_publish-dev:
-    unless: << pipeline.parameters.run-integration-tests >>
+  lint-pack:
     jobs:
-      - orb-tools/lint
-      # pack your orb YAML files to a single orb.yml
-      # validate the orb.yml file to ensure it is well-formed
-      - orb-tools/pack
-
-      # release dev version of orb, for testing & possible publishing.
-      # orb will be published as dev:alpha and dev:${CIRCLE_SHA1:0:7}.
-      # requires a CircleCI API token to be stored as CIRCLE_TOKEN (default)
-      # https://circleci.com/docs/2.0/managing-api-tokens
-      # store CIRCLE_TOKEN as a project env var or Contexts resource
-      # if using Contexts, add your context below
-      - orb-tools/publish-dev:
+      - orb-tools/lint:
+          filters: *filters
+      - orb-tools/pack:
+          filters: *filters
+      - orb-tools/review:
+          filters: *filters
+      - shellcheck/check:
+          exclude: SC2148,SC2038,SC2086,SC2002,SC2016
+          filters: *filters
+      - orb-tools/publish:
           orb-name: circleci/ruby
-          context: orb-publishing
+          vcs-type: << pipeline.project.type >>
           requires:
-            - orb-tools/pack
-            - orb-tools/lint
-
-      # trigger an integration workflow to test the
-      # dev:${CIRCLE_SHA1:0:7} version of your orb
-      - orb-tools/trigger-integration-tests-workflow:
-          name: trigger-integration-dev
+            [orb-tools/lint, orb-tools/review, orb-tools/pack, shellcheck/check]
+          # Use a context to hold your publishing token.
           context: orb-publishing
-          requires:
-            - orb-tools/publish-dev
-
-  # This `integration-tests_prod-release` workflow will only run
-  # when the run-integration-tests pipeline parameter is set to true.
-  # It is meant to be triggered by the "trigger-integration-tests-workflow"
-  # job, and run tests on <your orb>@dev:${CIRCLE_SHA1:0:7}.
-  integration-tests_prod-release:
-    when: << pipeline.parameters.run-integration-tests >>
-    jobs:
-      - integration-tests
-      - install-on-machine
-      - install-on-node
-      # your integration test jobs go here: essentially, run all your orb's
-      # jobs and commands to ensure they behave as expected. or, run other
-      # integration tests of your choosing
-      - orb-tools/dev-promote-prod-from-commit-subject:
-          orb-name: circleci/ruby
-          context: orb-publishing
-          add-pr-comment: true
-          bot-token-variable: GHI_TOKEN
-          bot-user: cpe-bot
-          fail-if-semver-not-indicated: true
-          publish-version-tag: true
-          ssh-fingerprints: c3:5b:58:1f:cd:05:64:a7:8c:b4:d9:19:be:9f:85:1f
-          requires:
-            - install-on-machine
-            - integration-tests
-            - install-on-node
-          filters:
-            branches:
-              only: master
+          filters: *filters
+      # Triggers the next workflow in the Orb Development Kit.
+      - orb-tools/continue:
+          pipeline-number: << pipeline.number >>
+          vcs-type: << pipeline.project.type >>
+          requires: [orb-tools/publish]
+          filters: *filters

.yamllint
@@ -1,0 +1,6 @@
+extends: relaxed
+
+rules:
+    line-length:
+        max: 200
+        allow-non-breakable-inline-mappings: true

src/commands/install-deps.yml
@@ -37,41 +37,10 @@ steps:
   - run:
       name: Bundle Install <<^parameters.with-cache>>(No Cache)<</parameters.with-cache>>
       working_directory: <<parameters.app-dir>>
-      command: |
-        if test -f "Gemfile.lock"; then
-          APP_BUNDLER_VERSION=$(cat Gemfile.lock | tail -1 | tr -d " ")
-          if [ -z "$APP_BUNDLER_VERSION" ]; then
-            echo "Could not find bundler version from Gemfile.lock. Please use bundler-version parameter"
-          else
-            echo "Gemfile.lock is bundled with bundler version $APP_BUNDLER_VERSION"
-          fi
-        fi
-
-        if ! [ -z <<parameters.bundler-version>> ]; then
-          echo "Found bundler-version parameter to override"
-          APP_BUNDLER_VERSION=<<parameters.bundler-version>>
-        fi
-
-        if ! echo $(bundle version)  | grep -q $APP_BUNDLER_VERSION; then
-          echo "Installing bundler $APP_BUNDLER_VERSION"
-          gem install bundler:$APP_BUNDLER_VERSION
-        else
-          echo "bundler $APP_BUNDLER_VERSION is already installed."
-        fi
-
-        if bundle config set > /dev/null 2>&1; then
-          if [ "<< parameters.path >>" == "./vendor/bundle" ]; then
-            bundle config deployment 'true'
-          fi
-          bundle config path << parameters.path >>
-        else
-          if [ "<< parameters.path >>" == "./vendor/bundle" ]; then
-            bundle config set deployment 'true'
-          fi
-          bundle config set path << parameters.path >>
-        fi
-
-        bundle check || bundle install
+      environment:
+        PARAM_PATH: << parameters.path >>
+        PARAM_BUNDLER_VERSION: << parameters.bundler-version >>
+      command: << include(scripts/install-deps.sh) >>
   - when:
       condition: <<parameters.with-cache>>
       steps:

src/commands/install.yml
@@ -6,59 +6,10 @@ parameters:
 steps:
   - run:
       name: "Install/Verify Ruby Version Manager."
-      command: |
-        # Disable IPv6
-        mkdir -p ~/.gnupg/
-        find ~/.gnupg -type d -exec chmod 700 {} \;
-        echo "disable-ipv6" >> ~/.gnupg/dirmngr.conf
-
-        count=0
-        until gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-        do
-            count=$((count+1)); sleep 10;
-            if [ $count -gt 2 ]; then
-                echo "Unable to receive GPG keys, FAILING";
-                exit 1;
-            fi;
-            echo "Network error: Unable to receive GPG keys. Will attempt again ($count/3)";
-        done;
-        ## Update if RVM is installed and exit
-        if [ -x "$(command -v rvm -v)" ]; then
-            rvm get stable
-            exit 0
-        fi
-
-        curl -sSL "https://get.rvm.io" | bash -s stable
-
-        # check for machine image specific path
-        if [ -d /opt/circleci/.rvm ]; then
-          echo "Setting PATH up for system install"
-          # this should be what needs to be added to that $BASH_ENV since this is what's in bash_profile - i dont know when $HOME is set
-          echo 'export PATH=$PATH:/opt/circleci/.rvm/bin' >> $BASH_ENV
-          echo "source /opt/circleci/.rvm/scripts/rvm" >> $BASH_ENV
-          # this will source if anyone logs in noninteractively, nvm setup only adds nvm to the path, to get the rubygems later you need to source this again
-          echo "source /opt/circleci/.rvm/scripts/rvm" >> ~/.bashrc
-        else
-          # Most circle builds run as a root user, in which case rvm gets installed in /usr/local/rvm instead of $HOME/.rvm
-          RVM_HOME=$HOME/.rvm
-          if [ -f "$RVM_HOME/scripts/rvm" ]; then
-            echo "Using $RVM_HOME"
-          else
-            RVM_HOME=/usr/local/rvm
-            echo "Using $RVM_HOME"
-          fi
-
-          echo "Setting PATH up for local install"
-          # this should be what needs to be added to that $BASH_ENV since this is what's in bash_profile - i dont know when $HOME is set
-          echo 'export PATH=$PATH:$RVM_HOME/bin' >> $BASH_ENV
-          echo "source $RVM_HOME/scripts/rvm" >> $BASH_ENV
-          # this will source if anyone logs in noninteractively, nvm setup only adds nvm to the path, to get the rubygems later you need to source this again
-          echo "source $RVM_HOME/scripts/rvm" >> ~/.bashrc
-        fi
+      command: << include(scripts/install-rvm.sh) >>
 
   - run:
       name: "Install Ruby v<< parameters.version >> via RVM"
-      command: |
-        rvm install << parameters.version >>
-        rvm use << parameters.version >>
-        echo . $(rvm <<parameters.version>> do rvm env --path) >> $BASH_ENV
+      environment:
+        PARAM_VERSION: << parameters.version >>
+      command: << include(scripts/install-ruby.sh) >>

src/commands/rubocop-check.yml
@@ -20,10 +20,18 @@ parameters:
       Path to the directory containing your Gemfile file. Not needed if Gemfile lives in the root.
     default: .
     type: string
+  parallel:
+    description: >
+      Use available CPUs to execute inspection in parallel.
+    default: false
+    type: boolean
 steps:
   - run:
       name: <<parameters.label>>
-      command: |
-        mkdir -p <<parameters.out-path>>
-        bundle exec rubocop <<parameters.check-path>> --out <<parameters.out-path>>/check-results.xml --format <<parameters.format>>
+      environment:
+        PARAM_OUT_PATH: <<parameters.out-path>>
+        PARAM_CHECK_PATH: <<parameters.check-path>>
+        PARAM_FORMAT: <<parameters.format>>
+        PARAM_PARALLEL: <<parameters.parallel>>
+      command: <<include(scripts/rubocop-check.sh)>>
       working_directory: <<parameters.app-dir>>

src/scripts/install-deps.sh
@@ -1,0 +1,36 @@
+#!/usr/bin/env bash
+
+if test -f "Gemfile.lock"; then
+  APP_BUNDLER_VERSION=$(cat Gemfile.lock | tail -1 | tr -d " ")
+  if [ -z "$APP_BUNDLER_VERSION" ]; then
+    echo "Could not find bundler version from Gemfile.lock. Please use bundler-version parameter"
+  else
+    echo "Gemfile.lock is bundled with bundler version $APP_BUNDLER_VERSION"
+  fi
+fi
+
+if [ -n "$PARAM_BUNDLER_VERSION" ]; then
+  echo "Found bundler-version parameter to override"
+  APP_BUNDLER_VERSION="$PARAM_BUNDLER_VERSION"
+fi
+
+if bundle version | grep -q $APP_BUNDLER_VERSION; then
+  echo "Installing bundler $APP_BUNDLER_VERSION"
+  gem install bundler:$APP_BUNDLER_VERSION
+else
+  echo "bundler $APP_BUNDLER_VERSION is already installed."
+fi
+
+if bundle config set > /dev/null 2>&1; then
+  if [ "$PARAM_PATH" == "./vendor/bundle" ]; then
+    bundle config deployment 'true'
+  fi
+  bundle config path "$PARAM_PATH"
+else
+  if [ "$PARAM_PATH" == "./vendor/bundle" ]; then
+    bundle config set deployment 'true'
+  fi
+  bundle config set path "$PARAM_PATH"
+fi
+
+bundle check || bundle install

src/scripts/install-ruby.sh
@@ -1,0 +1,7 @@
+#!/usr/bin/env bash
+
+rvm install "$PARAM_VERSION"
+rvm use "$PARAM_VERSION"
+
+readonly ruby_path="$(rvm $PARAM_VERSION 1> /dev/null 2> /dev/null && rvm env --path)"
+printf '%s\n' "source $ruby_path" >> $BASH_ENV

src/scripts/install-rvm.sh
@@ -1,0 +1,50 @@
+#!/usr/bin/env bash
+
+# Disable IPv6
+mkdir -p ~/.gnupg/
+find ~/.gnupg -type d -exec chmod 700 {} \;
+echo "disable-ipv6" >> ~/.gnupg/dirmngr.conf
+
+count=0
+until gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+do
+    count=$((count+1)); sleep 10;
+    if [ $count -gt 2 ]; then
+        echo "Unable to receive GPG keys, FAILING";
+        exit 1;
+    fi;
+    echo "Network error: Unable to receive GPG keys. Will attempt again ($count/3)";
+done;
+## Update if RVM is installed and exit
+if [ -x "$(command -v rvm -v)" ]; then
+    rvm get stable
+    exit 0
+fi
+
+curl -sSL "https://get.rvm.io" | bash -s stable
+
+# check for machine image specific path
+if [ -d /opt/circleci/.rvm ]; then
+  echo "Setting PATH up for system install"
+  # this should be what needs to be added to that $BASH_ENV since this is what's in bash_profile - i dont know when $HOME is set
+  echo 'export PATH=$PATH:/opt/circleci/.rvm/bin' >> $BASH_ENV
+  echo "source /opt/circleci/.rvm/scripts/rvm" >> $BASH_ENV
+  # this will source if anyone logs in noninteractively, nvm setup only adds nvm to the path, to get the rubygems later you need to source this again
+  echo "source /opt/circleci/.rvm/scripts/rvm" >> ~/.bashrc
+else
+  # Most circle builds run as a root user, in which case rvm gets installed in /usr/local/rvm instead of $HOME/.rvm
+  RVM_HOME=$HOME/.rvm
+  if [ -f "$RVM_HOME/scripts/rvm" ]; then
+    echo "Using $RVM_HOME"
+  else
+    RVM_HOME=/usr/local/rvm
+    echo "Using $RVM_HOME"
+  fi
+
+  echo "Setting PATH up for local install"
+  # this should be what needs to be added to that $BASH_ENV since this is what's in bash_profile - i dont know when $HOME is set
+  echo 'export PATH=$PATH:$RVM_HOME/bin' >> $BASH_ENV
+  echo "source $RVM_HOME/scripts/rvm" >> $BASH_ENV
+  # this will source if anyone logs in noninteractively, nvm setup only adds nvm to the path, to get the rubygems later you need to source this again
+  echo "source $RVM_HOME/scripts/rvm" >> ~/.bashrc
+fi

src/scripts/rubocop-check.sh
@@ -1,0 +1,14 @@
+#!/usr/bin/env bash
+
+mkdir -p "$PARAM_OUT_PATH"
+
+if [ "$PARAM_PARALLEL" -eq 0 ]; then
+  bundle exec rubocop "$PARAM_CHECK_PATH" \
+  --out $"$PARAM_OUT_PATH"/check-results.xml \
+  --format "$PARAM_FORMAT"
+else
+  bundle exec rubocop "$PARAM_CHECK_PATH" \
+  --out $"$PARAM_OUT_PATH"/check-results.xml \
+  --format "$PARAM_FORMAT" \
+  --parallel
+fi

.circleci/test-deploy.yml
@@ -1,0 +1,123 @@
+version: 2.1
+orbs:
+  ruby: circleci/ruby@dev:<<pipeline.git.revision>>
+  orb-tools: circleci/orb-tools@11.1
+
+filters: &filters
+  tags:
+    only: /.*/
+
+executors:
+  ubuntu:
+    parameters:
+      image:
+        description: Enter the version that should be user for the machine executor.
+        type: string  
+    machine: 
+      image: << parameters.image >>
+
+jobs:
+  integration-tests:
+    docker:
+      - image: cimg/ruby:2.7-node
+      - image: cimg/postgres:14.2
+        environment:
+          POSTGRES_USER: circleci-demo-ruby
+          POSTGRES_DB: rails_blog_test
+          POSTGRES_PASSWORD: ""
+    parallelism: 2
+    environment:
+      BUNDLE_JOBS: "3"
+      BUNDLE_RETRY: "3"
+      PGHOST: 127.0.0.1
+      PGUSER: circleci-demo-ruby
+      PGPASSWORD: ""
+      RAILS_ENV: test
+    steps:
+      - run: git clone https://github.com/CircleCI-Public/circleci-demo-ruby-rails.git .
+      - ruby/install-deps:
+          key: "gems-v4"
+      - restore_cache:
+          keys:
+            - rails-demo-yarn-v3-{{ checksum "yarn.lock" }}
+      - run:
+          name: Yarn Install
+          command: yarn install --cache-folder ~/.cache/yarn
+      # Store yarn / webpacker cache
+      - save_cache:
+          key: rails-demo-yarn-v3-{{ checksum "yarn.lock" }}
+          paths:
+            - ~/.cache/yarn
+      - run:
+          name: Wait for DB
+          command: dockerize -wait tcp://localhost:5432 -timeout 1m
+      - run:
+          name: Database setup
+          command: bundle exec rails db:schema:load --trace
+      - ruby/rspec-test:
+          label: RSpec Tests with Single Glob
+      - ruby/rspec-test:
+          label: RSpec Tests with Multiple Globs
+          include: "spec/**/*_spec.rb,spec/**/*_spec.rb"
+      - ruby/rspec-test:
+          label: RSpec Tests with seed
+          order: "rand:1234"
+      - ruby/rubocop-check:
+          label: Run Rubocop default parameters.
+      - ruby/rubocop-check:
+          label: Run Rubocop with the parallel flag.
+          parallel: true
+  install-on-machine:
+    parameters:
+      image:
+        description: Enter the version that should be user for the machine executor.
+        type: string  
+    executor: 
+      name: ubuntu
+      image: << parameters.image >>
+    steps:
+      - ruby/install:
+          version: "2.7.0"
+      - run:
+          name: "Test Install"
+          command: ruby --version | grep "2.7.0"
+  install-on-node:
+      docker:
+        - image: cimg/node:current
+      steps:
+        - ruby/install:
+            version: "2.6"
+        - run:
+            name: "Test Install"
+            command: ruby --version | grep "2.6"
+
+workflows:
+  test-deploy:
+    jobs:
+      # Make sure to include "filters: *filters" in every test job you want to run as part of your deployment.
+      - integration-tests:
+          filters: *filters
+      - install-on-machine:
+          matrix:
+            parameters:
+              image: [ubuntu-2004:202111-02]
+          filters: *filters
+      - install-on-node:
+          filters: *filters
+      - orb-tools/pack:
+          filters: *filters
+      - orb-tools/publish:
+          orb-name: circleci/ruby
+          vcs-type: << pipeline.project.type >>
+          pub-type: production
+          requires:
+            - orb-tools/pack
+            - integration-tests
+            - install-on-machine
+            - install-on-node
+          context: orb-publishing
+          filters:
+            branches:
+              ignore: /.*/
+            tags:
+              only: /^v[0-9]+\.[0-9]+\.[0-9]+$/

src/commands/rspec-test.yml
@@ -2,7 +2,9 @@ description: "Test with RSpec. You have to add `gem 'rspec_junit_formatter'` to 
 parameters:
     include:
         default: "spec/**/*_spec.rb"
-        description: Glob to define where your test files are kept within your repository.
+        description: >
+            Glob to define where your test files are kept within your repository.
+            Should multiple globs be required, they must be passed in a comma separated string (e.g.: "spec/**/*_spec.rb,spec2/**/*_spec.rb").
         type: string
     label:
         default: "RSpec Tests"
@@ -17,13 +19,20 @@ parameters:
             Path to the directory containing your Gemfile file. Not needed if Gemfile lives in the root.
         default: .
         type: string
+    order:
+        default: "defined"
+        description: >
+            Use the order parameter to tell RSpec how to order the files, groups, and examples.
+            Available options can be found at: https://relishapp.com/rspec/rspec-core/docs/command-line/order
+        type: string
 steps:
     - run:
         name: <<parameters.label>>
-        command: |
-            mkdir -p <<parameters.out-path>>
-            TESTFILES=$(circleci tests glob "<<parameters.include>>" | circleci tests split --split-by=timings)
-            bundle exec rspec $TESTFILES --profile 10 --format RspecJunitFormatter --out <<parameters.out-path>>/results.xml --format progress
+        environment:
+            PARAM_OUT_PATH: <<parameters.out-path>>
+            PARAM_INCLUDE: <<parameters.include>>
+            PARAM_ORDER: <<parameters.order>>
+        command: <<include(scripts/rspec-test.sh)>>
         working_directory: <<parameters.app-dir>>
     - store_test_results:
         path: <<parameters.out-path>>

src/scripts/rspec-test.sh
@@ -1,0 +1,38 @@
+#!/usr/bin/env bash
+
+if [ "$CIRCLE_NODE_TOTAL" -eq 1 ]; then
+  printf '%s\n' "Your job parallelism is set to 1."
+  printf '%s\n' "The split test by timings requires at least 2 nodes to generate historical timing data."
+  printf '%s\n' "Consider increasing your job parallelism to 2 or more."
+  printf '%s\n' "See https://circleci.com/docs/2.0/parallelism-faster-jobs/#using-the-circleci-cli-to-split-tests for more information."
+fi
+
+# Disable bash glob expansion
+# Without this, the glob parameter will be expanded before the split command is run
+set -o noglob
+
+if ! mkdir -p "$PARAM_OUT_PATH"; then
+  printf '%s\n' "Failed to create output directory: \"$PARAM_OUT_PATH\""
+  exit 1
+fi
+
+# Backup IFS
+readonly old_ifs="$IFS"
+
+# Split globs per comma and run the CLI split command
+IFS="," 
+read -ra globs <<< "$PARAM_INCLUDE"
+split_files=$(circleci tests glob "${globs[@]}" | circleci tests split --split-by=timings)
+
+# Convert list of test files to array
+# This is necessary because the split command returns a list of files separated by newline
+while IFS= read -r line; do test_files+=("$line"); done <<< "$split_files"
+
+# Rollback IFS
+IFS="$old_ifs"
+
+# Parse array of test files to string separated by single space and run tests
+# Leaving set -x here because it's useful for debugging what files are being tested
+set -x
+bundle exec rspec "${test_files[@]}" --profile 10 --format RspecJunitFormatter --out "$PARAM_OUT_PATH"/results.xml --format progress --order "$PARAM_ORDER"
+set +x