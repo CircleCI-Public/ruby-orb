73b7b6bb403c7c2ea99e48fab6b82d1294ac3e2c
Kelvin Tay <kelvintaywl@gmail.com>
2022-04-19 09:42:33 +0900

    feat: allow evaluation of `version` param
    This will allow support of users passing in env vars, like `'${MY_RUBY_VERSION}'` or things like `'$(cat foo/bar/.ruby-version)'`.

--- src/scripts/install-ruby.sh
+++ src/scripts/install-ruby.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-rvm install "$PARAM_VERSION"
-rvm use "$PARAM_VERSION"
+PARAM_RUBY_VERSION=$(eval echo "${PARAM_VERSION}")
 
-readonly ruby_path="$(rvm $PARAM_VERSION 1> /dev/null 2> /dev/null && rvm env --path)"
+rvm install "$PARAM_RUBY_VERSION"
+rvm use "$PARAM_RUBY_VERSION"
+
+readonly ruby_path="$(rvm $PARAM_RUBY_VERSION 1> /dev/null 2> /dev/null && rvm env --path)"
 printf '%s\n' "source $ruby_path" >> $BASH_ENV
