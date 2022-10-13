c03127e608f7657a88f19a92811b3801666c0e95
Jaryt Bustard <jarytbustard@gmail.com>
2022-10-13 12:17:37 -0400

    fix: Shellcheck error and out of date orb publish context (#109)
    * chore: remove readonly to resolve shellcheck error
    
    * ci: updated publishing context

--- src/scripts/install-ruby.sh
+++ src/scripts/install-ruby.sh
@@ -5,5 +5,5 @@ PARAM_RUBY_VERSION=$(eval echo "${PARAM_VERSION}")
 rvm install "$PARAM_RUBY_VERSION"
 rvm use "$PARAM_RUBY_VERSION"
 
-readonly ruby_path="$(rvm $PARAM_RUBY_VERSION 1> /dev/null 2> /dev/null && rvm env --path)"
-printf '%s\n' "source $ruby_path" >> $BASH_ENV
+RUBY_PATH="$(rvm $PARAM_RUBY_VERSION 1> /dev/null 2> /dev/null && rvm env --path)"
+printf '%s\n' "source $RUBY_PATH" >> $BASH_ENV
