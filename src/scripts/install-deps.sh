f17733928e0b284a330c82fd59abdc8a9999fbd8
kelvintaywl <kelvintaywl@gmail.com>
2024-02-14 10:49:02 +0900

    set RVM_HOME accordingly, and update --with-openssl-dir to use it

--- src/scripts/install-deps.sh
+++ src/scripts/install-deps.sh
@@ -7,9 +7,19 @@ if bundle config set > /dev/null 2>&1; then
   bundle config gemfile "$PARAM_GEMFILE"
   bundle config path "$PARAM_PATH"
 
-  if [ -d "$HOME/.rvm/usr/ssl" ]; then
-    echo "Detected rvm ssl version. Configuring bundle package with openssl dir $HOME/.rvm/usr."
-    bundle config build.openssl --with-openssl-dir="$HOME/.rvm/usr"
+  if [ -d /opt/circleci/.rvm ]; then
+    RVM_HOME=/opt/circleci/.rvm
+  else
+    # Most circle builds run as a root user, in which case rvm gets installed in /usr/local/rvm instead of $HOME/.rvm
+    RVM_HOME=$HOME/.rvm
+    if [ ! -f "$RVM_HOME/scripts/rvm" ]; then
+      RVM_HOME=/usr/local/rvm
+    fi
+  fi
+
+  if [ -d "$RVM_HOME/usr/ssl" ]; then
+    echo "Detected rvm ssl version. Configuring bundle package with openssl dir $RVM_HOME/usr."
+    bundle config build.openssl --with-openssl-dir="$RVM_HOME/usr"
   fi
 else
   if [ "$PARAM_PATH" == "./vendor/bundle" ]; then
@@ -18,9 +28,9 @@ else
   bundle config set gemfile "$PARAM_GEMFILE"
   bundle config set path "$PARAM_PATH"
 
-  if [ -d "$HOME/.rvm/usr/ssl" ]; then
-    echo "Detected rvm ssl version. Configuring bundle package with openssl dir $HOME/.rvm/usr."
-    bundle config set build.openssl --with-openssl-dir="$HOME/.rvm/usr"
+  if [ -d "$RVM_HOME/usr/ssl" ]; then
+    echo "Detected rvm ssl version. Configuring bundle package with openssl dir $RVM_HOME/usr."
+    bundle config set build.openssl --with-openssl-dir="$RVM_HOME/usr"
   fi
 fi
 
