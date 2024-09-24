12c38c3b5d5f153adbb8d8d3c06ff818da240879
Masumi Kawasaki <geeknees@gmail.com>
2024-09-25 00:57:39 +0900

    The --no-parallel option does not exist prior to v1.13.0, and the default is no parallel execution, so the option is not passed

--- src/scripts/rubocop-check.sh
+++ src/scripts/rubocop-check.sh
@@ -2,13 +2,24 @@
 
 mkdir -p "$PARAM_OUT_PATH"
 
+RUBOCOP_VERSION=$(bundle exec rubocop -v)
+RUBOCOP_VERSION_MAJOR=$(echo "$RUBOCOP_VERSION" | cut -d. -f1)
+RUBOCOP_VERSION_MINOR=$(echo "$RUBOCOP_VERSION" | cut -d. -f2)
+
 if [ "$PARAM_PARALLEL" -eq 1 ]; then
   bundle exec rubocop "$PARAM_CHECK_PATH" \
   --format "$PARAM_FORMAT" \
+  --parallel \
   --out $"$PARAM_OUT_PATH"/check-results.xml
 else
-  bundle exec rubocop "$PARAM_CHECK_PATH" \
-  --format "$PARAM_FORMAT" \
-  --no-parallel \
-  --out $"$PARAM_OUT_PATH"/check-results.xml
+  if [ "$RUBOCOP_VERSION_MAJOR" -gt 1 ] || { [ "$RUBOCOP_VERSION_MAJOR" -eq 1 ] && [ "$RUBOCOP_VERSION_MINOR" -ge 13 ]; }; then
+    bundle exec rubocop "$PARAM_CHECK_PATH" \
+    --format "$PARAM_FORMAT" \
+    --no-parallel \
+    --out $"$PARAM_OUT_PATH"/check-results.xml
+  else
+    bundle exec rubocop "$PARAM_CHECK_PATH" \
+    --format "$PARAM_FORMAT" \
+    --out $"$PARAM_OUT_PATH"/check-results.xml
+  fi
 fi
