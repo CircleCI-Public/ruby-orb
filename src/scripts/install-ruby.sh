a95742868da46443f33f2da7b6ebe15a1919bdc1
Mateo Arboleda <mateo.arboleda@circleci.com>
2024-11-19 10:27:42 -0500

    Use rvm head

--- src/scripts/install-ruby.sh
+++ src/scripts/install-ruby.sh
@@ -2,8 +2,11 @@
 
 PARAM_RUBY_VERSION=$(eval echo "${PARAM_VERSION}")
 RUBY_VERSION_MAJOR=$(echo "$PARAM_VERSION" | cut -d. -f1)
+RUBY_VERSION_MINOR=$(echo "$PARAM_VERSION" | cut -d. -f2)
 detected_platform="$(uname -s | tr '[:upper:]' '[:lower:]')"
-if [ "$detected_platform" = "darwin" ] && [ "$RUBY_VERSION_MAJOR" -le 2 ]; then
+
+# When on MacOS, and versions minor or equal to 3.0.x. These are the versions depending on OpenSSL 1.1
+if [ "$detected_platform" = "darwin" ] && {[ "$RUBY_VERSION_MAJOR" -le 2 ] || {[ "$RUBY_VERSION_MAJOR" -eq 3 ] && [ "$RUBY_VERSION_MINOR" -eq 0 ]}}; then
     brew install openssl@1.1
     OPENSSL_LOCATION="$(brew --prefix openssl@1.1)"
     export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$OPENSSL_LOCATION"
