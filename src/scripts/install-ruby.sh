d100504b3e13d0a00cb9427121040287f9c5571e
Mateo Arboleda <mateo.arboleda@circleci.com>
2024-11-19 13:22:30 -0500

    Remove openssl 1.1 installation for old versions

--- src/scripts/install-ruby.sh
+++ src/scripts/install-ruby.sh
@@ -1,20 +1,16 @@
 #!/usr/bin/env bash
 
 PARAM_RUBY_VERSION=$(eval echo "${PARAM_VERSION}")
-# RUBY_VERSION_MAJOR=$(echo "$PARAM_VERSION" | cut -d. -f1)
-# RUBY_VERSION_MINOR=$(echo "$PARAM_VERSION" | cut -d. -f2)
-# detected_platform="$(uname -s | tr '[:upper:]' '[:lower:]')"
+RUBY_VERSION_MAJOR=$(echo "$PARAM_VERSION" | cut -d. -f1)
+RUBY_VERSION_MINOR=$(echo "$PARAM_VERSION" | cut -d. -f2)
+detected_platform="$(uname -s | tr '[:upper:]' '[:lower:]')"
 
 # When on MacOS, and versions minor or equal to 3.0.x. These are the versions depending on OpenSSL 1.1
-# if [[ "$detected_platform" = "darwin" && ( "$RUBY_VERSION_MAJOR" -le 2 || ( "$RUBY_VERSION_MAJOR" -eq 3  &&  "$RUBY_VERSION_MINOR" -eq 0 ) ) ]]; then
-#     echo "Installing OpenSSL 1.1 with brew"
-#     brew install openssl@1.1
-#     OPENSSL_LOCATION="$(brew --prefix openssl@1.1)"
-#     export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$OPENSSL_LOCATION"
-#     rbenv install $PARAM_RUBY_VERSION
-#     rbenv global $PARAM_RUBY_VERSION
-#     exit 0
-# fi
+if [[ "$detected_platform" = "darwin" && ( "$RUBY_VERSION_MAJOR" -le 2 || ( "$RUBY_VERSION_MAJOR" -eq 3  &&  "$RUBY_VERSION_MINOR" -eq 0 ) ) ]]; then
+    rbenv install $PARAM_RUBY_VERSION
+    rbenv global $PARAM_RUBY_VERSION
+    exit 0
+fi
 
 if [ -n "$PARAM_OPENSSL_PATH" ]; then
     echo "Using path $PARAM_OPENSSL_PATH for OpenSSL"
@@ -25,7 +21,7 @@ elif ! openssl version | grep -q -E '1\.[0-9]+\.[0-9]+'; then
     # location of RVM is expected to be available at RVM_HOME env var
     WITH_OPENSSL="--with-openssl-dir=$RVM_HOME/usr"
 fi
-
+rvm get master
 rvm install "$PARAM_RUBY_VERSION" "$WITH_OPENSSL"
 rvm use "$PARAM_RUBY_VERSION"
 
