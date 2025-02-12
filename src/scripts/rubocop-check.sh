01e5d6509f71f84db8131c67efe989321aa13958
marboledacci <mateo.arboleda@circleci.com>
2025-02-12 15:55:24 -0500

    Feat/custom rubocop output (#158)
    * Allow to customize the name of the output file for rubocop
    
    * Updat tests to check format json

--- src/scripts/rubocop-check.sh
+++ src/scripts/rubocop-check.sh
@@ -10,16 +10,16 @@ if [ "$PARAM_PARALLEL" -eq 1 ]; then
   bundle exec rubocop "$PARAM_CHECK_PATH" \
   --format "$PARAM_FORMAT" \
   --parallel \
-  --out $"$PARAM_OUT_PATH"/check-results.xml
+  --out $"$PARAM_OUT_PATH"/"$PARAM_FILE_OUT"
 else
   if [ "$RUBOCOP_VERSION_MAJOR" -gt 1 ] || { [ "$RUBOCOP_VERSION_MAJOR" -eq 1 ] && [ "$RUBOCOP_VERSION_MINOR" -ge 13 ]; }; then
     bundle exec rubocop "$PARAM_CHECK_PATH" \
     --format "$PARAM_FORMAT" \
     --no-parallel \
-    --out $"$PARAM_OUT_PATH"/check-results.xml
+    --out $"$PARAM_OUT_PATH"/"$PARAM_FILE_OUT"
   else
     bundle exec rubocop "$PARAM_CHECK_PATH" \
     --format "$PARAM_FORMAT" \
-    --out $"$PARAM_OUT_PATH"/check-results.xml
+    --out $"$PARAM_OUT_PATH"/"$PARAM_FILE_OUT"
   fi
 fi
