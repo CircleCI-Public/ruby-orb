4acdafdddef798eeec8220aed2f8373122b7178c
marboledacci <mateo.arboleda@circleci.com>
2025-03-20 10:09:59 -0500

    Update openssl installation method (#160)
    * Add tests for newer versions of ruby
    
    * Add tests for docker
    
    * Use debian instead of alpine
    
    * Install curl
    
    * apt update
    
    * fix
    
    * Install gpg
    
    * Add more dependencies
    
    * Verify openssl
    
    * Update validation
    
    * use autolib for openssl
    
    * Add a post step and test a new way for openssl
    
    * Fix script
    
    * fix syntax
    
    * Update directory to run post-steps
    
    * Update directory to run post-steps
    
    * Only verify openssl version on post-steps
    
    * Validate with previous version
    
    * Install openssl based on ruby version
    
    * fix syntax for version condition
    
    * use env subst
    
    * Add debug
    
    * Fix usage of variables
    
    * Add more info to test

--- src/scripts/install-ruby.sh
+++ src/scripts/install-ruby.sh
@@ -1,26 +1,30 @@
 #!/usr/bin/env bash
-
-PARAM_RUBY_VERSION=$(eval echo "${PARAM_VERSION}")
-RUBY_VERSION_MAJOR=$(echo "$PARAM_VERSION" | cut -d. -f1)
-RUBY_VERSION_MINOR=$(echo "$PARAM_VERSION" | cut -d. -f2)
+PARAM_RUBY_VERSION="$(echo "$PARAM_VERSION" | circleci env subst)"
+RUBY_VERSION_MAJOR=$(echo "$PARAM_RUBY_VERSION" | cut -d. -f1)
+RUBY_VERSION_MINOR=$(echo "$PARAM_RUBY_VERSION" | cut -d. -f2)
 detected_platform="$(uname -s | tr '[:upper:]' '[:lower:]')"
 
 # When on MacOS, and versions minor or equal to 3.0.x. These are the versions depending on OpenSSL 1.1
-if [[ "$detected_platform" = "darwin" && ( "$RUBY_VERSION_MAJOR" -le 2 || ( "$RUBY_VERSION_MAJOR" -eq 3  &&  "$RUBY_VERSION_MINOR" -eq 0 ) ) ]]; then
-    rbenv install $PARAM_RUBY_VERSION
-    rbenv global $PARAM_RUBY_VERSION
-    exit 0
+if [[ "$RUBY_VERSION_MAJOR" -le 2 || ( "$RUBY_VERSION_MAJOR" -eq 3  &&  "$RUBY_VERSION_MINOR" -eq 0 ) ]]; then
+    if [[ "$detected_platform" = "darwin" ]]; then
+        rbenv install $PARAM_RUBY_VERSION
+        rbenv global $PARAM_RUBY_VERSION
+        exit 0
+    else
+        if [ -n "$PARAM_OPENSSL_PATH" ]; then
+            echo "Using path $PARAM_OPENSSL_PATH for OpenSSL"
+            WITH_OPENSSL="--with-openssl-dir=$PARAM_OPENSSL_PATH"
+        elif ! openssl version | grep -q -E '1\.[0-9]+\.[0-9]+'; then 
+            echo "Did not find supported openssl version. Installing Openssl rvm package."
+            rvm pkg install openssl
+            # location of RVM is expected to be available at RVM_HOME env var
+            WITH_OPENSSL="--with-openssl-dir=$RVM_HOME/usr"
+        fi
+    fi
+else
+    rvm autolibs enable
 fi
 
-if [ -n "$PARAM_OPENSSL_PATH" ]; then
-    echo "Using path $PARAM_OPENSSL_PATH for OpenSSL"
-    WITH_OPENSSL="--with-openssl-dir=$PARAM_OPENSSL_PATH"
-elif ! openssl version | grep -q -E '1\.[0-9]+\.[0-9]+'; then 
-    echo "Did not find supported openssl version. Installing Openssl rvm package."
-    rvm pkg install openssl
-    # location of RVM is expected to be available at RVM_HOME env var
-    WITH_OPENSSL="--with-openssl-dir=$RVM_HOME/usr"
-fi
 rvm get master
 rvm install "$PARAM_RUBY_VERSION" "$WITH_OPENSSL"
 rvm use "$PARAM_RUBY_VERSION"
